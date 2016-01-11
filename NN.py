aef71201001f9cb637ba90a270142f76252b1534
s <waved1980@gmail.com>
2016-01-10 21:57:05 -0500

    Initial upload

---
+++ NN.py
@@ -0,0 +1,13 @@
+class NN(object):
+	"""docstring for NN"""
+	def __init__(self, Structure, Bias):#Structure is an array, e.g. '[5,15,1]',Bias is an array, e.g. '[1,1]'
+		super(NN, self).__init__()
+		self.Structure = Structure
+		self.Bias = Bias
+	def init_SWM():
+		numberoflayers=len(Structure)
+		for i in Structure 	# loop through each layer
+			swm.append(rand(ns(i-1)+(~bn(i-1)==0),ns(i))*2-1)
+		self.swm=
+		pass
+		
